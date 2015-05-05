5b0df794b8e2b0f1c49e994692f9c4254e806c59
Adam Balan <a-kyle@hotmail.com>
2015-05-04 19:10:38 -0600

    Fixed indenting.

--- app/Controllers/SessionController.php
+++ app/Controllers/SessionController.php
@@ -22,28 +22,28 @@ class SessionController implements \Lib\Controller\BaseController  {
 
 
     public static function createAction($params){
-      $postParams = $params->request()->post();
-      $flash = new Flash();
+        $postParams = $params->request()->post();
+        $flash = new Flash();
 
-      $user = getEntityManager()->getRepository('\ImageUploader\Models\User');
+        $user = getEntityManager()->getRepository('\ImageUploader\Models\User');
 
-      $userObject = $user->findBy(array('user_name' => $postParams['username']));
+        $userObject = $user->findBy(array('user_name' => $postParams['username']));
 
-      if (empty($userObject)) {
-        $flash->createFlash('error', 'User does not exist.');
-        $params->redirect('/signin');
-      }
+        if (empty($userObject)) {
+            $flash->createFlash('error', 'User does not exist.');
+            $params->redirect('/signin');
+        }
 
-      if (!$userObject[0]->checkPassword($postParams['password'])) {
-        $flash->createFlash('error', 'Invalid Password');
-        $params->redirect('/signin');
-      }
+        if (!$userObject[0]->checkPassword($postParams['password'])) {
+            $flash->createFlash('error', 'Invalid Password');
+            $params->redirect('/signin');
+        }
 
-      $_SESSION[KEY . $userObject[0]->getId()] = serialize($userObject[0]->getUserName());
-      $_SESSION[KEY . $userObject[0]->getId() . '_created'] = time();
+        $_SESSION[KEY . $userObject[0]->getId()] = serialize($userObject[0]->getUserName());
+        $_SESSION[KEY . $userObject[0]->getId() . '_created'] = time();
 
-      $flash->createFlash('success', 'Welcome back ' . $userObject[0]->getFirstName());
-      $params->redirect('/dashboard');
+        $flash->createFlash('success', 'Welcome back ' . $userObject[0]->getFirstName());
+        $params->redirect('/dashboard');
 
     }
 
